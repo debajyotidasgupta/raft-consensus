b754b31637b831a4630dae1c1f853635e5e92ca0
debajyotidasgupta <debajyotidasgupta6@gmail.com>
2022-02-08 12:41:29 +0530

    Create New Raft Node added

--- raft/raft.go
+++ raft/raft.go
@@ -2,6 +2,7 @@ package raft
 
 import (
 	"sync"
+	"time"
 )
 
 type RNState int // RNState is the state of the Raft node
@@ -25,25 +26,63 @@ type LogEntry struct {
 }
 
 type RaftNode struct {
-	id         uint64           // id is the id of the Raft node
-	mu         sync.Mutex       // Mutex protects the Raft node
-	state      RNState          // State is the state of the Raft node
-	peers      []uint64         // Peer is the list of peers in the Raft cluster
-	server     *Server          // Server is the server of the Raft node. Issue RPCs to the peers
-	db         Database         // Database is the storage of the Raft node
-	commitChan chan CommitEntry // CommitChan is the channel the channel where this Raft Node is going to report committed log entries
-	newCommit  chan CommitEntry // NewCommit is an internal notification channel used to notify that new log entries may be sent on commitChan.
-	trigger    chan struct{}    // Trigger is the channel used to trigger the Raft node to send a AppendEntries RPC to the peers when some relevant event occurs
+	id             uint64           // id is the id of the Raft node
+	mu             sync.Mutex       // Mutex protects the Raft node
+	peers          []uint64         // Peer is the list of peers in the Raft cluster
+	server         *Server          // Server is the server of the Raft node. Issue RPCs to the peers
+	db             *Database        // Database is the storage of the Raft node
+	commitChan     chan CommitEntry // CommitChan is the channel the channel where this Raft Node is going to report committed log entries
+	newCommitReady chan struct{}    // NewCommitReady is an internal notification channel used to notify that new log entries may be sent on commitChan.
+	trigger        chan struct{}    // Trigger is the channel used to trigger the Raft node to send a AppendEntries RPC to the peers when some relevant event occurs
 
 	// Persistent state on all servers
 	currentTerm uint64     // CurrentTerm is the current term of the Raft node
 	votedFor    uint64     // VotedFor is the candidate id that received a vote in the current term
 	log         []LogEntry // Log is the log of the Raft node
 
+	// IMPORTANT: Use 1 based indexing for log entries
+
 	// Volatile state on all servers
-	commitIndex uint64   // CommitIndex is the index of the last committed log entry
-	lastApplied uint64   // LastApplied is the index of the last applied log entry
-	nextIndex   []uint64 // NextIndex is the index of the next log entry to send to each peer
+	commitIndex        uint64    // CommitIndex is the index of the last committed log entry
+	lastApplied        uint64    // LastApplied is the index of the last applied log entry
+	state              RNState   // State is the state of the Raft node
+	electionResetEvent time.Time // ElectionResetEvent is the time at which the Raft node had last reset its election timer
+
+	// Volatile state on leaders
+	nextIndex  map[uint64]uint64 // NextIndex is the index of the next log entry to send to each peer
+	matchIndex map[uint64]uint64 // MatchIndex is the index of the highest log entry known to be replicated on the leader's peers
+}
+
+// NewRaftNode creates a new Raft node.
+func NewRaftNode(id uint64, peers []uint64, server *Server, db *Database, ready chan interface{}, commitChan chan CommitEntry) *RaftNode {
+	newRaftNode := &RaftNode{
+		id:                 id,
+		peers:              peers,
+		server:             server,
+		db:                 db,
+		commitChan:         commitChan,
+		newCommitReady:     make(chan struct{}, 16),
+		trigger:            make(chan struct{}, 1),
+		currentTerm:        0,
+		votedFor:           0,
+		log:                make([]LogEntry, 0),
+		commitIndex:        0,
+		lastApplied:        0,
+		state:              Follower,
+		electionResetEvent: time.Now(),
+		nextIndex:          make(map[uint64]uint64),
+		matchIndex:         make(map[uint64]uint64),
+	}
+
+	// Start the Raft node
+	go func() {
+		<-ready
+		newRaftNode.mu.Lock()
+		defer newRaftNode.mu.Unlock()
+		newRaftNode.electionResetEvent = time.Now()
+	}()
+
+	return newRaftNode
 }
 
 func (s RNState) String() string { // String returns the string representation of a Raft node state
