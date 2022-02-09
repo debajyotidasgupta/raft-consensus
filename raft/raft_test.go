2bc7e3ef88296c964e5ab1679bea9f820e877b07
debajyotidasgupta <debajyotidasgupta6@gmail.com>
2022-02-09 21:51:19 +0530

    fix test type to uint64

--- raft/raft_test.go
+++ raft/raft_test.go
@@ -24,16 +24,17 @@ func (a Addr) String() string {
 
 var wg = sync.WaitGroup{}
 
-var ports map[int]string = map[int]string{}
+var ports map[uint64]string = map[uint64]string{}
 
-func communicate(serverId int, numPeers int, s *Server) {
+func communicate(serverId uint64, numPeers uint64, s *Server) {
 	defer wg.Done()
 	for {
-		peerId := rand.Intn(numPeers) + 1
-		pause := rand.Intn(50)
-		pauseTime := pause * int(time.Millisecond)
+		peerId := uint64(rand.Intn(int(numPeers)) + 1)
+		pause := uint64(rand.Intn(50))
+		pauseTime := pause * uint64(time.Millisecond)
+
 		time.Sleep(time.Duration(pauseTime))
-		msg := rand.Intn(1000)
+		msg := uint64(rand.Intn(1000))
 		if peerId == serverId {
 			log.Println(serverId, "Shutting")
 			s.DisconnectAll()
@@ -43,7 +44,7 @@ func communicate(serverId int, numPeers int, s *Server) {
 		} else {
 			addr := Addr{"tcp", "[::]:" + ports[peerId]}
 			err := s.ConnectToPeer(peerId, addr)
-			var reply int
+			var reply uint64
 			if err == nil {
 				log.Printf("[%d] sending %d to [%d]\n", serverId, msg, peerId)
 				s.RPC(peerId, "ServiceType.DisplayMsg", msg, &reply)
@@ -59,10 +60,10 @@ func communicate(serverId int, numPeers int, s *Server) {
 
 // Change the numPeers to test with different number of peers
 func TestServerClient(t *testing.T) {
-	var numPeers int = 5
+	var numPeers uint64 = 5
 	var port = 20000
 
-	for i := 1; i <= numPeers; i++ {
+	for i := uint64(1); i <= numPeers; i++ {
 		portStr := strconv.Itoa(port)
 		ports[i] = portStr
 		port++
@@ -70,10 +71,10 @@ func TestServerClient(t *testing.T) {
 
 	var servers []*Server
 
-	for i := 1; i <= numPeers; i++ {
-		peerIds := make([]int, numPeers-1)
+	for i := uint64(1); i <= numPeers; i++ {
+		peerIds := make([]uint64, numPeers-1)
 		j := 0
-		for peerId := 1; peerId <= numPeers; peerId++ {
+		for peerId := uint64(1); peerId <= numPeers; peerId++ {
 			if peerId != i {
 				peerIds[j] = peerId
 				j++
@@ -87,7 +88,7 @@ func TestServerClient(t *testing.T) {
 		s.Serve(ports[i])
 	}
 
-	for i := 1; i <= numPeers; i++ {
+	for i := uint64(1); i <= numPeers; i++ {
 		wg.Add(1)
 		go communicate(i, numPeers, servers[i-1])
 	}
