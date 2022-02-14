80d23ad7abc1e46a2b9e54e13a31d5efdf78bf05
somnathjena2011 <somnathjena.2011@gmail.com>
2022-02-14 18:14:53 +0530

    some client code

--- main.go
+++ main.go
@@ -13,3 +13,12 @@ func Set(cs *raft.ClusterSimulator, serverId uint64, key string, value int) erro
 	}
 	return nil
 }
+
+func Get(cs *raft.ClusterSimulator, serverId uint64, key string, value *int) error {
+	readCommand := raft.Read{Key: key}
+	isLeader := cs.SubmitToServer(int(serverId), readCommand, value)
+	if !isLeader {
+		return fmt.Errorf("server not leader")
+	}
+	return nil
+}
